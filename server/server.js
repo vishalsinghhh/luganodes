dc4f866c63401b74faf4ef369ab0748218befc8e
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-08-02 03:26:02 +0530

    server done

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const DHT = require("hyperdht");
 const goodbye = require("graceful-goodbye");
 const b4a = require("b4a");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const app = express();
 const dht = new DHT();
@@ -12,61 +12,64 @@ const keyPair = DHT.keyPair();
 app.get("/", (req, res) => {
   res.send("Luganodes");
 });
-let userID=''
-let type = ""
+let userID = "";
+let type = "";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.post("/", (req, res)=>{
-    const {type_of_user, user_id} =req.body
-    userID = user_id
-    type = type_of_user
-    recallFn()
-    res.send("Hello")
-})
-let messages = []
-app.get('/messages', (req, res)=>{
-    res.send({messages})
-})
+app.post("/", (req, res) => {
+  const { type_of_user, user_id } = req.body;
+  userID = user_id;
+  type = type_of_user;
+  recallFn();
+  res.send("Hello");
+});
+let messages = [];
+app.get("/messages", (req, res) => {
+  res.send({ messages });
+});
 
 const port = process.env.PORT || 5000;
-const recallFn=()=>{
-    if (type === "main") {
-        const server = dht.createServer(async (conn) => {
-          console.log("got connection!");
-          await process.stdin.pipe(conn).pipe(process.stdout);
-        });
-        server.listen(keyPair).then(() => {
-          console.log("listening on:", b4a.toString(keyPair.publicKey, "hex"));
-        });
-      }
-      if (type === "client") {
-        console.log(
-          "Connecting to:",
-          userID
-        );
-        const publicKey = b4a.from(
-          userID,
-          "hex"
-        );
-        const conn = dht.connect(publicKey);
-        conn.once("open", () => console.log("got connection!"));
-        conn.on("data", (data) => {
-            const message = data.toString();
-            // console.log("Received message:", {"otherSide":message});
-            messages.push({"otherSide":message});
-          });
-      
-        process.stdin.pipe(conn).pipe(process.stdout);
-        process.stdin.on('data', data => {
-            const inputMessage = data.toString().trim();
-            if (inputMessage.length > 0) {
-              messages.push({"mySide":inputMessage});
-            //   console.log('Stored message:', inputMessage);
-            }
-          });
-      }
-}
+const recallFn = () => {
+  if (type === "main") {
+    const server = dht.createServer(async (conn) => {
+      console.log("got connection!");
+      await process.stdin.pipe(conn).pipe(process.stdout);
+      conn.on("data", (data) => {
+        const message = data.toString();
+        messages.push({ otherSide: message });
+      });
+    });
+    server.listen(keyPair).then(() => {
+      console.log("listening on:", b4a.toString(keyPair.publicKey, "hex"));
+      process.stdin.on("data", (data) => {
+        const inputMessage = data.toString().trim();
+        if (inputMessage.length > 0) {
+          messages.push({ mySide: inputMessage });
+        }
+      });
+    });
+  }
 
+  //   CLIENT SIDE
+  if (type === "client") {
+    console.log("Connecting to:", userID);
+    const publicKey = b4a.from(userID, "hex");
+    const conn = dht.connect(publicKey);
+    conn.once("open", () => console.log("got connection!"));
+    conn.on("data", (data) => {
+      const message = data.toString();
+      messages.push({ otherSide: message });
+    });
+
+    process.stdin.pipe(conn).pipe(process.stdout);
+    process.stdin.on("data", (data) => {
+      const inputMessage = data.toString().trim();
+      if (inputMessage.length > 0) {
+        messages.push({ mySide: inputMessage });
+      }
+    });
+  }
+};
 
 const start = async () => {
   try {
