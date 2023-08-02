b58bf84b3307d68576435e620975e856240580c4
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-08-02 18:45:24 +0530

    page

--- client/src/app/chat/[id]/page.js
+++ client/src/app/chat/[id]/page.js
@@ -1,18 +1,41 @@
-"use client"
-import React, { useEffect } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
 import axios from "axios";
 
 const page = () => {
+  const params = useParams();
+  const [newChats, setNewChats] = useState();
   const submitData = async () => {
-    const response = await axios.get("http://localhost:5000/messages")
-  }
-  useEffect(()=>{
-    submitData()
-  },[])
-  
+    const response = await axios.get("http://localhost:5000/messages");
+    setNewChats(response.data.messages);
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      submitData();
+    }, 1000);
+  }, []);
+  console.log(newChats);
+
   return (
     <div>
-      <div>chats</div>
+      <div>Chat Room - {params?.id}</div>
+      <div>
+        {newChats?.map((item) => {
+          return (
+            <div>
+              <div>
+                {Object.keys(item) == "otherSide" && (
+                  <div>{item.otherSide}</div>
+                )}
+              </div>
+              <div>
+                {Object.keys(item) == "mySide" && <div>{item.mySide}</div>}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
